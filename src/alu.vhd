8dc63248dd3fd9ffc86de93007400c7006533974
linuxlover3000 <charlieyonkura@gmail.com>
2025-11-23 22:06:18 -0500

    Fix: unclock ALU

--- src/alu.vhd
+++ src/alu.vhd
@@ -15,26 +15,24 @@ end ALU;
 
 architecture behavioral of ALU is
     begin
-        process(clk, op, A, B) is
-            if rising_edge(clk) then
-                case op is
-                    when "000" | "001" | "010" => -- lw, sw, add
-                        result <= std_logic_vector(signed(A) + signed(B));
-                    --when "001" => -- sw
-                    --    result <= std_logic_vector(signed(A) + signed(B));
-                    --when "010" => -- add
-                    --    result <= std_logic_vector(signed(A) + signed(B));
-                    --when "011" => -- bne
-                    --    result <= std_logic_vector(signed(A) - signed(B));
-                    when "100" => -- ori
-                        result <= A or B;
-                    when "101" => -- srl
-                        result <= A srl B;
-                    when others =>
-                        result <= (others => '0');
-                end case;
-                zero <= '1' when (unsigned(result) = 0) else '0';
-            end if;
+        process(op, A, B) is
+            case op is
+                when "000" | "001" | "010" => -- lw, sw, add
+                    result <= std_logic_vector(signed(A) + signed(B));
+                --when "001" => -- sw
+                --    result <= std_logic_vector(signed(A) + signed(B));
+                --when "010" => -- add
+                --    result <= std_logic_vector(signed(A) + signed(B));
+                --when "011" => -- bne
+                --    result <= std_logic_vector(signed(A) - signed(B));
+                when "100" => -- ori
+                    result <= A or B;
+                when "101" => -- srl
+                    result <= A srl B;
+                when others =>
+                    result <= (others => '0');
+            end case;
+            zero <= '1' when (unsigned(result) = 0) else '0';
         end process;
         
 
