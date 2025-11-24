2b112c15554549e89b6e7e02a7248b357a730fef
linuxlover3000 <charlieyonkura@gmail.com>
2025-11-24 00:25:22 -0500

    Data memory initialized to blank

--- src/data_mem.vhd
+++ src/data_mem.vhd
@@ -15,15 +15,7 @@ end DataMemory;
 
 architecture behavioral of DataMemory is
     type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
-    signal mem: memory_array := (   0 => x"",
-                                    1 => x"",
-                                    2 => x"",
-                                    3 => x"",
-                                    4 => x"",
-                                    5 => x"",
-                                    6 => x"",
-                                    7 => x"",
-                                    others => x"00000000");
+    signal mem: memory_array := (others => x"00000000");
 
     begin
         read_data <= mem(to_integer(unsigned(addr(7 downto 0))));
